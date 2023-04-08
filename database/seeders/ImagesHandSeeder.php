c8ab36518119b3061527bf863de9f78bf48e1f0f
itsukimori <itsukimorinaga@yahoo.co.jp>
2023-04-09 08:40:31 +0900

    seeder ref

--- database/seeders/ImagesHandSeeder.php
+++ database/seeders/ImagesHandSeeder.php
@@ -11,15 +11,17 @@ class ImagesHandSeeder extends Seeder
     public function run(): void
     {
         // public/drawing-images の画像をPathをDB保管処理
-        $folderPath= public_path('/drawing-images/hand/*');
+        $folderPath = public_path('/drawing-images/hand/*');
         $imageFullPath = glob($folderPath); // ←/var/www/html/public/test-image/hoge.png
-        // $imagesPath = str_replace('/var/www/html/public/drawing-images', '/drawing-images', $imageFullPath);
+
         foreach ($imageFullPath as $imagePath) {
+            $relativeImagePath = str_replace(public_path(), '', $imagePath);
             DB::table('images')->insert([
                 'body_parts' => 'hand',
-                'image_path' => $imagePath,
+                'image_path' => $relativeImagePath,
             ]);
         }
+
     }
 }
 

a5cdab512f9123f68dcfc665116c14eaaee8d89e
itsukimori <itsukimorinaga@yahoo.co.jp>
2023-04-23 23:12:07 +0900

    lesson content add

--- resources/views/layouts/illustLessonContent.blade.php
+++ resources/views/layouts/illustLessonContent.blade.php
@@ -156,6 +156,22 @@ nav ul{
         width: 337px;
     }
 }
+/* pinterest-content(レスポンシブ*埋め込み) */
+.pinterest-wrapper {
+  position: relative;
+  height: 400px;
+  overflow: hidden;
+}
+
+.pinterest-wrapper iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: 0;
+}
+
 </style>
 <body>
     @yield('content')
